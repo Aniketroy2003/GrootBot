8b7ec35646f9b33995ce7120cb0832ca487a714a
Aniket Roy <spacelover2003@gmail.com>
2024-02-14 18:51:58 +0530

    deployed

--- app.py
+++ app.py
@@ -1,16 +1,5 @@
 import streamlit as st
-# import pathlib
-# import textwrap
-
 import google.generativeai as genai
-
-# from IPython.display import display
-# from IPython.display import Markdown
-
-
-# def to_markdown(text):
-#   text = text.replace('•', '  *')
-#   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
 
 
 # Function to call OpenAI API and get response
@@ -24,7 +13,7 @@ def generate_response(prompt, api_key):
 def main():
     st.title("Groot (The chat bot)")
     st.markdown("How may I help you?")
-    # Input field for OpenAI API key
+
     api_key = "AIzaSyDVwznMhukxzTq9PAv67y-dD8TGnHKLa3M"
     
     # Input field for user's message
